8d4b13615f938b5d5bef4e3ef3737941b50a7e66
Wes Bassler <wes.bassler@emburse.com>
2021-05-11 11:03:28 -0400

    pipeline test 1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 
 app = Flask(__name__)
 
-VERSION = "dev-0.0.1"
+VERSION = "dev-0.0.2"
 
 # Set the Remote Tracking Server Information
 tracking_server = os.getenv("MLFLOW_TRACKING_URI", "http://mlflow")
